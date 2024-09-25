f8a2b975c703a554823cecc275b7bfb8c87715f6
Weldon Scott <wascott@uwaterloo.ca>
2024-09-24 20:33:34 -0500

    Add support for escaped characters in string literals

--- compiler/main.cc
+++ compiler/main.cc
@@ -14,27 +14,27 @@ int main() {
 
     program
         .add_include("print_num")
-        .add_code(print_str("Hello, world!") + print_str("Hello, world!") + print_str("Hello, world!") +
-                  (chunk1.push_chunk() +
-                   (movi(Register::scratch, 400) +
-                    chunk1.write_variable(x, Register::scratch)) +
-                   (movi(Register::scratch, 500) +
-                    chunk1.write_variable(y, Register::scratch))) +
-                  chunk2.push_chunk() +
-                  movi(Register::scratch, 123456789098599595) +
-                  chunk2.write_variable(x, Register::scratch) +
-                  movi(Register::scratch, 200) +
-                  chunk2.write_variable(y, Register::scratch) +
-                  chunk2.read_variable(Register::scratch, x) +
-                  print_num(Register::scratch) +
-                  chunk2.read_variable(Register::scratch, y) +
-                  print_num(Register::scratch) +
-                  chunk2.pop_chunk() +
-                  chunk1.read_variable(Register::scratch, x) +
-                  print_num(Register::scratch) +
-                  chunk1.read_variable(Register::scratch, y) +
-                  print_num(Register::scratch) +
-                  chunk1.pop_chunk())
+        .add_code(print_str("Hello, worl\"d!\n") + print_str("Hello, world!\n") + print_str("Hello, world!\n"))
+        //   (chunk1.push_chunk() +
+        //    (movi(Register::scratch, 400) +
+        //     chunk1.write_variable(x, Register::scratch)) +
+        //    (movi(Register::scratch, 500) +
+        //     chunk1.write_variable(y, Register::scratch))) +
+        //   chunk2.push_chunk() +
+        //   movi(Register::scratch, 123456789098599595) +
+        //   chunk2.write_variable(x, Register::scratch) +
+        //   movi(Register::scratch, 200) +
+        //   chunk2.write_variable(y, Register::scratch) +
+        //   chunk2.read_variable(Register::scratch, x) +
+        //   print_num(Register::scratch) +
+        //   chunk2.read_variable(Register::scratch, y) +
+        //   print_num(Register::scratch) +
+        //   chunk2.pop_chunk() +
+        //   chunk1.read_variable(Register::scratch, x) +
+        //   print_num(Register::scratch) +
+        //   chunk1.read_variable(Register::scratch, y) +
+        //   print_num(Register::scratch) +
+        //   chunk1.pop_chunk())
         .compile()
         .run();
 
--- compiler/modules/popipo.cc
+++ compiler/modules/popipo.cc
@@ -1,11 +1,28 @@
 #include "modules/popipo.h"
 
 #include <format>
-#include <iostream>
+#include <map>
 
 #include "core/instruction.h"
 #include "core/program.h"
 #include "core/transformer.h"
+
+const std::map<char, char> escape_map{
+    {'\'', '\''}, {'\"', '\"'}, {'\?', '?'}, {'\\', '\\'}, {'\a', 'a'}, {'\b', 'b'}, {'\f', 'f'}, {'\n', 'n'}, {'\r', 'r'}, {'\t', 't'}, {'\v', 'v'}};
+
+const std::string escape(const std::string& str) {
+    std::string escaped;
+    for (size_t i = 0; i < str.size(); i++) {
+        if (escape_map.contains(str[i])) {
+            escaped += '\\';
+            escaped += escape_map.at(str[i]);
+        } else {
+            escaped += str[i];
+        }
+    }
+
+    return escaped;
+}
 
 class PrintStr : public Code {
     const std::string str;
@@ -21,8 +38,6 @@ class PrintStr : public Code {
 
 class LiteralTransformer : public SingletonTransformer<LiteralTransformer> {
     Line apply(Line line, Program& program) const override;
-
-   public:
 };
 
 PrintStr::PrintStr(const std::string& str) : Code(false, {LiteralTransformer::instance()}), str{str} {}
@@ -39,7 +54,7 @@ Line LiteralTransformer::apply(Line line, Program& program) const {
         return line;
     }
 
-    std::pair<const std::string, const std::string> labels{program.add_literal(print_str_ptr->str)};
+    std::pair<const std::string, const std::string> labels{program.add_literal(escape(print_str_ptr->str))};
 
     return push_pair(Register(0), Register(1)) + push_pair(Register(2), Register(8)) +  // preserve values of x0, x1, x2, x8
            movi(Register(0), 1) +
