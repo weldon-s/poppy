ef11c3cfe6befde9c184860769bf3033b3e21fc0
Weldon Scott <wascott@uwaterloo.ca>
2024-10-12 13:47:03 -0500

    Nullable checks

--- compiler/language/grammar.cc
+++ compiler/language/grammar.cc
@@ -1,8 +1,25 @@
 #include "grammar.h"
 
+#include <algorithm>
+
 #include "symbol.h"
 
-Grammar::Grammar(Symbol start, std::vector<Rule> rules) : _start{start}, _rules{rules} {}
+Grammar::Grammar(Symbol start, std::vector<Rule> rules) : _start{start}, _rules{rules}, _nullable{} {
+    for (size_t i = 0; i < _rules.size(); ++i) {
+        bool current_nullable = true;
+
+        for (const Symbol& s : _rules[i].rhs()) {
+            if (std::find(_nullable.begin(), _nullable.end(), s) == _nullable.end()) {
+                current_nullable = false;
+                break;
+            }
+        }
+
+        if (current_nullable && (std::find(_nullable.begin(), _nullable.end(), _rules[i].lhs()) == _nullable.end())) {
+            _nullable.push_back(_rules[i].lhs());
+        }
+    }
+}
 
 Symbol Grammar::start() const {
     return _start;
@@ -10,4 +27,8 @@ Symbol Grammar::start() const {
 
 const std::vector<Rule>& Grammar::rules() const {
     return _rules;
+}
+
+bool Grammar::nullable(Symbol s) const {
+    return std::find(_nullable.begin(), _nullable.end(), s) != _nullable.end();
 }
--- compiler/language/grammar.h
+++ compiler/language/grammar.h
@@ -9,11 +9,13 @@
 class Grammar {
     const Symbol _start;
     const std::vector<Rule> _rules;
+    std::vector<Symbol> _nullable;
 
    public:
     Grammar(Symbol start, std::vector<Rule> rules);
     Symbol start() const;
     const std::vector<Rule>& rules() const;
+    bool nullable(Symbol s) const;
 };
 
 #endif
