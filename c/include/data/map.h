cd9f33e19c5d56d4f9700753ce781a264fafd9bb
Weldon Scott <wascott@uwaterloo.ca>
2025-07-26 12:24:19 -0400

    Implement map macros

---
+++ c/include/data/map.h
@@ -0,0 +1,66 @@
+#ifndef MAP_H
+#define MAP_H
+#include "data/list.h"
+
+#define MAP_ENTRY(ktype, vtype) ktype##_##vtype##_map_entry
+#define MAP(ktype, vtype) ktype##_##vtype##_map
+#define LISTMAP_ENTRY(ktype, vtype) ktype##_##vtype##_map_entry_list
+#define LIST_NODEMAP_ENTRY(ktype, vtype) ktype##_##vtype##_map_entry_list_node
+
+#define DEFINE_MAP(ktype, vtype)                                    \
+        struct MAP_ENTRY(ktype, vtype) {                            \
+                struct ktype *key;                                  \
+                struct vtype *value;                                \
+        };                                                          \
+        DEFINE_LIST(MAP_ENTRY(ktype, vtype))                        \
+        struct MAP(ktype, vtype) {                                  \
+                struct LISTMAP_ENTRY(ktype, vtype) *list;           \
+                bool (*equals)(struct ktype*, struct ktype*);       \
+                void (*free_func)(struct MAP_ENTRY(ktype, vtype)*); \
+        };                                                          \
+
+#define init_map(map, eq, ff, ktype, vtype)                                                                               \
+        do {                                                                                                              \
+                map->list = (struct LISTMAP_ENTRY(ktype, vtype)*) malloc(sizeof(struct LISTMAP_ENTRY(ktype, vtype)));     \
+                init_list(map->list);                                                                                     \
+                map->equals = eq;                                                                                         \
+                map->free_func = ff;                                                                                      \
+        } while (0);
+
+#define update_map(map, k, v, ktype, vtype)                                                                                                       \
+        do {                                                                                                                                      \
+                struct MAP_ENTRY(ktype, vtype) *entry = (struct MAP_ENTRY(ktype, vtype)*) malloc(sizeof(struct MAP_ENTRY(ktype, vtype)));         \
+                entry->key = k;                                                                                                                   \
+                entry->value = v;                                                                                                                 \
+                bool found = false;                                                                                                               \
+                for (struct LIST_NODEMAP_ENTRY(ktype, vtype) *node = map->list->head; node != NULL; node = node->next) {                        \
+                        if (map->equals(k, node->data->key)) {                                                                                    \
+                                map->free_func(node->data);                                                                                       \
+                                node->data = entry;                                                                                               \
+                                found = true;                                                                                                     \
+                                break;                                                                                                            \
+                        }                                                                                                                         \
+                }                                                                                                                                 \
+                if (!found) {                                                                                                                     \
+                        append_list(map->list, entry, MAP_ENTRY(ktype, vtype))                                                                    \
+                }                                                                                                                                 \
+        } while (0);           
+
+#define query_map(map, k, result, ktype, vtype)                                                                            \
+        do {                                                                                                               \
+                result = NULL;                                                                                             \
+                for (struct LIST_NODEMAP_ENTRY(ktype, vtype) *node = map->list->head; node != NULL; node = node->next) {   \
+                        if (map->equals(k, node->data->key)){                                                              \
+                                result = node->data->value;                                                                \
+                                break;                                                                                     \
+                        }                                                                                                  \
+                }                                                                                                          \
+        } while (0);                                                                                                       \
+
+#define free_map(map, ktype, vtype)                                            \
+        do {                                                                   \
+                free_list(map->list, map->free_func, MAP_ENTRY(ktype, vtype)); \
+                free(map->list);                                               \
+        } while (0);                                                           \
+
+#endif
--- c/src/main.c
+++ c/src/main.c
@@ -1,53 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#include "data/map.h"
 #include "data/list.h"
 #include "lang/lexer.h"
 #include "lang/parser.h"
 #include "lang/poppy_grammar.h"
 
-void print(const struct parse_tree *pt, size_t level){
-        for(size_t i = 0; i < level; ++i){
-                printf("  ");
-        }
+// void print(const struct parse_tree *pt, size_t level){
+//         for(size_t i = 0; i < level; ++i){
+//                 printf("  ");
+//         }
 
-        printf("%s", symbol_name(pt->data.type));
+//         printf("%s", symbol_name(pt->data.type));
 
-        if (pt->data.value != NULL){
-                printf(" %s", pt->data.value);
-        }
-        printf("\n");
+//         if (pt->data.value != NULL){
+//                 printf(" %s", pt->data.value);
+//         }
+//         printf("\n");
 
-        if (pt->children != NULL){
-                for (struct LIST_NODE(parse_tree) *node = pt->children->head; node != NULL; node = node->next){
-                        print(node->data, level + 1);
-                }
-        }
+//         if (pt->children != NULL){
+//                 for (struct LIST_NODE(parse_tree) *node = pt->children->head; node != NULL; node = node->next){
+//                         print(node->data, level + 1);
+//                 }
+//         }
+// }
+
+// int main(int argc, char *argv[]){
+//         FILE *f = fopen("prog.pop", "r");
+//         struct LIST(token) *list = lex(f);
+//         fclose(f);
+
+//         if (list == NULL){
+//                 return 0;
+//         }
+
+//         const struct grammar *poppy_grammar = get_poppy_grammar();
+//         const struct parse_tree *pt = parse(poppy_grammar, list);
+//         free_poppy_grammar();
+
+//         if (pt == NULL) {
+//                 free_list(list, free_token, token);
+//                 free(list);
+//                 return 0;
+//         }
+
+//         print(pt, 0);
+
+//         free_list(list, free_token, token);
+//         free(list);
+//         free_parse_tree(pt);
+//         return 0;
+// }
+
+struct i {
+        int num;
+};
+
+struct d {
+        double num;
+};
+
+DEFINE_MAP(i, d);
+
+bool equals_i(struct i *i1, struct i *i2){
+        return i1->num == i2->num;
 }
 
-int main(int argc, char *argv[]){
-        FILE *f = fopen("prog.pop", "r");
-        struct LIST(token) *list = lex(f);
-        fclose(f);
+void free_entry(struct MAP_ENTRY(i, d) *entry){
+        free(entry->key);
+        free(entry->value);
+        free(entry);
+}
 
-        if (list == NULL){
-                return 0;
+int main (int argc, char *argv[]){
+        struct MAP(i, d) map;
+        init_map((&map), equals_i, free_entry, i, d);
+
+        for (int i = 1; i <= 10; ++i){
+                struct i *key = (struct i*) malloc(sizeof(struct i));
+                key->num = i;
+                struct d *value = (struct d*) malloc(sizeof(struct d));
+                value->num = 1.0 / i;
+                update_map((&map), key, value, i, d);
         }
 
-        const struct grammar *poppy_grammar = get_poppy_grammar();
-        const struct parse_tree *pt = parse(poppy_grammar, list);
-        free_poppy_grammar();
+        struct i *key = (struct i*) malloc(sizeof(struct i));
+        struct d *value = (struct d*) malloc(sizeof(struct d));
+        key->num = 5;
+        value->num = 0.5;
+        update_map((&map), key, value, i, d);
+        key = (struct i*) malloc(sizeof(struct i));
+        value = (struct d*) malloc(sizeof(struct d));
+        key->num = 12;
+        value->num = 1;
+        update_map((&map), key, value, i, d);
 
-        if (pt == NULL) {
-                free_list(list, free_token, token);
-                free(list);
-                return 0;
+        for (int i = 1; i <= 12; ++i){
+                struct i c = {i};
+                struct d *r;
+                query_map((&map), (&c), r, i, d);
+                r ? printf("%f\n", r->num) : printf("null\n");
         }
 
-        print(pt, 0);
-
-        free_list(list, free_token, token);
-        free(list);
-        free_parse_tree(pt);
-        return 0;
+        free_map((&map), i, d);
 }
